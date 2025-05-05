4649c4b75444d428c0178e83d7db7faa1d16a5db
Akash <akash.kumar@digio.in>
2025-05-05 13:50:42 +0530

    digio SDK intigartion as parent app

---
+++ app/build.gradle.kts
@@ -0,0 +1,114 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.test.myapplication"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.test.myapplication"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+//        compose = true
+        viewBinding = true
+        dataBinding = true
+
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // digio's
+    implementation(platform("com.github.digio-tech:digio-bom:v1.0.17"))
+    implementation("com.github.digio-tech:gateway")
+
+    implementation("androidx.appcompat:appcompat")
+    implementation("com.google.android.material:material")
+    implementation("androidx.navigation:navigation-fragment-ktx")
+    implementation("androidx.navigation:navigation-ui-ktx")
+
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout")
+    implementation("com.android.volley:volley")
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+    implementation("com.google.code.gson:gson")
+    implementation("com.github.digio-tech:digio_permissions")
+    implementation("com.google.android.gms:play-services-location")
+
+    implementation("com.github.digio-tech:sdk_offlinekyc")
+    implementation("com.github.digio-tech:protean-esign")
+
+    implementation("com.github.digio-tech:cvl_esign")
+    implementation("com.scottyab:rootbeer-lib")
+    implementation("com.afollestad.material-dialogs:core")
+
+    implementation("com.github.digio-tech:esign_biometrics")
+    implementation("com.github.digio-tech:cvl_rdservice")
+
+    implementation("com.github.digio-tech:esign_mandate")
+
+    implementation("com.github.digio-tech:sdk_native_camera")
+    implementation("com.github.digio-tech:image_processor")
+    implementation("androidx.exifinterface:exifinterface")
+
+    implementation("com.github.digio-tech:sdk_ml_camera")
+    implementation("com.google.mlkit:face-detection")
+    implementation("androidx.camera:camera-core")
+    implementation("androidx.camera:camera-camera2")
+    implementation("androidx.camera:camera-lifecycle")
+    implementation("androidx.camera:camera-view")
+    implementation("androidx.camera:camera-video")
+    implementation("androidx.preference:preference-ktx")
+
+    implementation("com.github.digio-tech:sdk_upi")
+    implementation("androidx.core:core-ktx")
+    implementation("com.squareup.retrofit2:retrofit")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("androidx.fragment:fragment-ktx")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx")
+    implementation("com.squareup.retrofit2:converter-gson")
+
+}
---
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -0,0 +1,166 @@
+package com.test.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+import com.test.myapplication.databinding.ActivityMainBinding
+import `in`.digio.sdk.gateway.Digio
+import `in`.digio.sdk.gateway.enums.DigioEnvironment
+import `in`.digio.sdk.gateway.enums.DigioServiceMode
+import `in`.digio.sdk.gateway.event.model.GatewayEvent
+import `in`.digio.sdk.gateway.interfaces.DigioSuccessFailureInterface
+import `in`.digio.sdk.gateway.model.DigioConfig
+import `in`.digio.sdk.gateway.model.DigioSdkResponse
+import `in`.digio.sdk.gateway.model.DigioTheme
+import `in`.digio.sdk.gateway.model.OtherParams
+
+class MainActivity : ComponentActivity(), DigioSuccessFailureInterface {
+    val digio = Digio()
+    val digioConfig = DigioConfig()
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set default values
+        binding.documentIdEdit.setText("KID250505132944334KUIS6CVAX2FXXX") // enter your Request ID
+        binding.emailMobileEdit.setText("abc@gamil.com") // identifier email/phone
+        binding.gwtEdit.setText("GWT250505132944375A1FLUGEI6R1XXS") // token optional
+
+        binding.serviceModeEdit.setText(DigioServiceMode.OTP.toString())
+        binding.envEdit.setText(DigioEnvironment.PRODUCTION.toString())
+        setServiceModeDropDown()
+        setEnvironmentDropDown()
+        binding.signNowBtm.setOnClickListener {
+            val documentId = binding.documentIdEdit.text?.toString().orEmpty()
+            val emailMobile = binding.emailMobileEdit.text?.toString().orEmpty()
+
+            if (documentId.isEmpty() || emailMobile.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Please fill in all forms",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                signNow()
+            }
+        }
+
+        val otherParams = OtherParams().apply {
+            // whitelabelType = "footer"
+        }
+        digioConfig.otherParams = otherParams
+
+        val theme = DigioTheme().apply {
+            // primaryColor = android.R.color.holo_red_dark
+            primaryColorHex = "#0261B0"
+            fontFamily = "Unbounded"
+            secondaryColorHex = "#141414"
+            fontUrl = "https://fonts.googleapis.com/css2?family=Unbounded:wght@200&display=swap"
+            // fontFormat = ""
+        }
+        digioConfig.theme = theme
+        digioConfig.faqButton = android.R.drawable.ic_menu_help
+        digioConfig.closeButton = android.R.drawable.ic_menu_close_clear_cancel
+        digioConfig.logo = "https://www.digio.in/images/digio_blue.png" // Your company logo URL
+        digioConfig.linkApproach = false
+
+        digioConfig.environment = DigioEnvironment.SANDBOX // SANDBOX or PRODUCTION
+        digioConfig.serviceMode = DigioServiceMode.OTP
+
+        digio.initSession(this@MainActivity, this)
+
+    }
+
+    private fun signNow() {
+        try {
+            digioConfig.gToken = binding.gwtEdit.text.toString()
+            digioConfig.requestId = binding.documentIdEdit.text.toString()
+            digioConfig.userIdentifier = binding.emailMobileEdit.text.toString()
+
+            digio.startSession(
+                digioConfig,
+                this
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    private fun setServiceModeDropDown() {
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.service_modes,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.serviceModeSpiner.adapter = adapter
+
+        binding.serviceModeSpiner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                binding.serviceModeEdit.setText(DigioServiceMode.valueOf(selectedItem).toString())
+                digioConfig.serviceMode = DigioServiceMode.valueOf(selectedItem)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                binding.serviceModeEdit.setText(DigioServiceMode.OTP.toString())
+                digioConfig.serviceMode = DigioServiceMode.OTP
+            }
+        }
+    }
+
+    private fun setEnvironmentDropDown() {
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.environment,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.environmentSpiner.adapter = adapter
+
+        binding.environmentSpiner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                val env = DigioEnvironment.valueOf(selectedItem)
+                binding.envEdit.setText(env.toString())
+                digioConfig.environment = env
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                binding.envEdit.setText(DigioEnvironment.PRODUCTION.toString())
+                digioConfig.environment = DigioEnvironment.PRODUCTION
+            }
+        }
+    }
+
+
+
+    override fun onFailure(sdkResponse: DigioSdkResponse?) {
+        binding.response.setText(sdkResponse.toString())
+        Log.d(TAG, "onFailure: $sdkResponse")
+    }
+
+    override fun onSuccess(sdkResponse: DigioSdkResponse?) {
+        binding.response.setText(sdkResponse.toString())
+        Log.d(TAG, "onSuccess: " + sdkResponse);
+    }
+
+    override fun onUpdateEvent(event: GatewayEvent?) {
+        binding.update.setText(event.toString())
+        Log.d(TAG, "onUpdateEvent: " + event);
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
