861164984736fbd9d164374b274eed30dbc3d3a6
Akash <akash.kumar@digio.in>
2026-04-22 17:24:56 +0530

    Missing dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,16 +64,18 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    // digio's
-    implementation(platform("com.github.digio-tech:digio-bom:v1.0.59"))
-    implementation("com.github.digio-tech:gateway")
-
     implementation("androidx.appcompat:appcompat")
     implementation("com.google.android.material:material")
-//    implementation ("com.google.android.material:material:1.11.0")
-
     implementation("androidx.navigation:navigation-fragment-ktx")
     implementation("androidx.navigation:navigation-ui-ktx")
+//
+//
+    implementation(platform("com.github.digio-tech:digio-bom:v1.0.66"))
+//    implementation(platform("com.github.digio-tech:digio-bom:v1.0.40-beta-2"))
+//
+    implementation("com.github.digio-tech:gateway")
+    implementation ("com.github.digio-tech:digioStateless")
+    implementation("com.github.digio-tech:api_client")
 
     implementation("androidx.swiperefreshlayout:swiperefreshlayout")
     implementation("com.android.volley:volley")
@@ -83,31 +85,52 @@ dependencies {
     implementation("com.github.digio-tech:digio_permissions")
     implementation("com.google.android.gms:play-services-location")
 
+//    implementation("com.github.digio-tech:sdk_offlinekyc")
+    implementation("org.simpleframework:simple-xml:2.7.1")
+    // Latest stable version for Glide
+    implementation("com.github.bumptech.glide:glide")
+
     implementation("com.github.digio-tech:sdk_offlinekyc")
+//    implementation("org.simpleframework:simple-xml") {
+//        exclude group: 'xpp3', module: 'xpp3'
+//        exclude group: 'stax', module: 'stax-api'
+//        exclude group: 'stax', module: 'stax'
+//    }
+
+
+//    implementation("com.github.digio-tech:esign_otp")
     implementation("com.github.digio-tech:protean-esign")
+//    implementation 'com.github.digio-tech:protean-esign'
 
     implementation("com.github.digio-tech:cvl_esign")
     implementation("com.scottyab:rootbeer-lib")
-    implementation("com.afollestad.material-dialogs:core")
-
+    implementation("com.afollestad.material-dialogs:core") //giving duplicate class with upi
+//    implementation 'com.afollestad.material-dialogs:core:3.3.0'
     implementation("com.github.digio-tech:esign_biometrics")
     implementation("com.github.digio-tech:cvl_rdservice")
 
+
     implementation("com.github.digio-tech:esign_mandate")
 
-
+//    implementation("com.github.digio-tech:sdk_native_camera")
     implementation("com.github.digio-tech:image_processor")
     implementation("androidx.exifinterface:exifinterface")
-    
-    implementation("com.github.digio-tech:sdk_ml_camera")
-    implementation("com.google.android.gms:play-services-mlkit-face-detection")
 
+
+    implementation("com.github.digio-tech:sdk_ml_camera")
+//    implementation("com.google.mlkit:face-detection")
+    implementation("com.google.android.gms:play-services-mlkit-face-detection")
     implementation("androidx.camera:camera-core")
     implementation("androidx.camera:camera-camera2")
     implementation("androidx.camera:camera-lifecycle")
     implementation("androidx.camera:camera-view")
     implementation("androidx.camera:camera-video")
     implementation("androidx.preference:preference-ktx")
+    implementation("androidx.exifinterface:exifinterface")
+    implementation("com.github.digio-tech:api_client")
+
+    implementation ("com.airbnb.android:lottie:6.6.6")
+
 
     implementation("com.github.digio-tech:sdk_upi")
     implementation("androidx.core:core-ktx")
@@ -117,29 +140,29 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx")
     implementation("com.squareup.retrofit2:converter-gson")
-
-    implementation ("com.airbnb.android:lottie:6.6.6")
-    implementation("androidx.core:core-animation:1.0.0")
-    implementation ("com.github.digio-tech:api_client:v5.0.1")
     implementation("io.coil-kt:coil:2.6.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx")
-    implementation("androidx.constraintlayout:constraintlayout")
+//    implementation 'androidx.activity:activity:1.11.0'
+//    implementation 'androidx.constraintlayout:constraintlayout:2.2.1'
+    implementation("org.simpleframework:simple-xml") {
+        exclude(group = "xmlpull", module = "xmlpull")
+        exclude(group = "xpp3", module = "xpp3")
+    }
 
-    // emudhra
-//    implementation("com.emudhra:esign:1.0.0-SNAPSHOT") {
-//        isChanging = true
-//    }
-//
-//    implementation("com.squareup.retrofit2:converter-simplexml:2.5.0") {
+    implementation("com.android.volley:volley") {
+        exclude(group = "xmlpull", module = "xmlpull")
+    }
+
+// implementation("com.github.digio-tech:two_way_video")
+
+//    implementation("live.videosdk:rtc-android-sdk") {
+//        exclude(group = "org.json", module = "json")
 //        exclude(group = "xmlpull", module = "xmlpull")
-//        exclude(group = "xpp3", module = "xpp3")
 //    }
-//
-//
-//    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.15.1"){
-//        exclude(group = "xpp3", module = "xpp3")
-//        exclude(group = "org.xmlpull", module = "xmlpull")
-//        exclude(group = "javax.xml.stream", module = "stax-api")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx")
+//    implementation("com.github.amitshekhariitbhu.Fast-Android-Networking:android-networking")
+    implementation("com.auth0:java-jwt:4.5.1") // to generate JWT token
+//    implementation('com.emudhra:esign:1.0.0-SNAPSHOT') {
+//        changing = true
 //    }
 
 }
